424a32d5238a060c8a096d3467b913810da790ae
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-01-29 13:10:41 +0530

    Added : Delete Data and Show Size of LinkedList

--- src/main/java/com/bridgelabz/linkedlist/LinkedList.java
+++ src/main/java/com/bridgelabz/linkedlist/LinkedList.java
@@ -20,15 +20,17 @@ public class LinkedList {
 		}
 	}
 	public void insert(Object data, Object key) {
-	        Node newNode = new Node(data);
-	        Node current = head;
-	        while (current.data != key) {
-	            current = current.next;
-	        }
-	        newNode.next = current.next;
-	        current.next = newNode;
-	    }
+	    
+		Node newNode = new Node(data);
+		Node current = head;
+		while (current.data != key) {
+			current = current.next;
+		}
+		newNode.next = current.next;
+		current.next = newNode;
+	}
 	public void Search(Object searchData) {
+	
 		if(head.data == searchData)
 			System.out.println(searchData+" is Found");
 		else {
@@ -47,7 +49,35 @@ public class LinkedList {
 				System.out.println(searchData+" is Not Found");
 		}
 	}
-	
+	public void Pop(Object data, Object key) {
+		
+		Node current = head;
+        Node prev = null;
+		if (current != null && current.data == key) {
+            head = current.next;
+            return;
+        }
+
+        while (current != null && current.data != key) {
+            prev = current;
+            current = current.next;
+        }
+
+        if (current == null) {
+            return;
+        }
+
+        prev.next = current.next;
+    }
+	public void size() {
+		int count = 0;
+		Node temp = head;
+		while(temp != null) {
+			count++;
+			temp = temp.next;
+		}
+		System.out.println("Size of LinkedList : "+count);
+	}
 	public void Display() {
 		Node temp = head;
 		while(temp != null) {
@@ -63,11 +93,13 @@ public class LinkedList {
 		
 		LinkedList list = new LinkedList();
 		list.Add(70);
+		list.Add(40);
+		list.Add(30);
 		list.Add(56);
-		list.insert(30, 56);
 		list.Display();
-		list.Search(30);
-		list.insert(40, 30);
+		list.Search(40);
+		list.Pop(30,40);
 		list.Display();
+		list.size();
 	}
 }
